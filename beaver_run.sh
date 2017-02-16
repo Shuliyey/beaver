1fd894d91267a7ead7718c088936d115346c0834
Zeyu Ye <z.ye@base2services.com>
2017-02-17 09:10:35 +1100

    add data_path to beaver_run.sh

--- beaver_run.sh
+++ beaver_run.sh
@@ -2,6 +2,7 @@ dir_name=$(cd `dirname $0` && pwd)
 params=$@
 tag=latest
 conf_path=${dir_name}/beaver.conf
+data_path=${dir_name}/data
 name=beaver
 net=bridge
 debug=false
@@ -23,6 +24,9 @@ for param in $params; do
       --conf_path)
         conf_path=${param_array[1]}
 	;;
+      --data_path)
+        data_path=${param_array[1]}
+	;;
       --name)
         name=${param_array[1]}
 	;;
@@ -37,10 +41,11 @@ for param in $params; do
 done
 
 if [ "$debug" == "true" ]; then
-  echo "tag=$tag, config_path=$config_path, name=$name, net=$net"
+  echo "tag=$tag, config_path=$config_path, data_path=$data_path, name=$name, net=$net"
 fi
 
 docker run -it -d --name=$name --restart=always \
   --net=$net \
   -v $conf_path:/opt/beaver/beaver.conf \
+  -v $data_path:/data \
   shuliyey/beaver:$tag
